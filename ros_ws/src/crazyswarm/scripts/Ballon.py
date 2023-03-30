bd8bd2183792927cac6e4be31aee325bff0d886c
Edward Jeffs <positronic83@gmail.com>
2023-03-30 16:11:18 -0400

    Added visual feedback control

---
+++ ros_ws/src/crazyswarm/scripts/Ballon.py
@@ -0,0 +1,37 @@
+
+import paho.mqtt.client as mqtt
+import rospy
+from std_msgs.msg import Float64MultiArray
+
+clientAddress = "192.168.0.5"
+MQTTHOST = "192.168.0.36"
+MQTTPORT = 1883
+MQTTTOPIC = "ballon"
+mqttClient = mqtt.Client()
+pub = rospy.Publisher('openmv', Float64MultiArray, queue_size=1)
+rospy.init_node('talker', anonymous=True)
+past = rospy.get_rostime().to_sec()-1
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    mqttClient.subscribe(MQTTTOPIC)
+
+def on_message(client, userdata, msg):
+    ballon = str(msg.payload.decode("utf-8"))
+    val = [eval(i) for i in ballon.split(' ')]
+    #print(round(val[0]/240,2), round(val[1]/160,2), val[2])#240*160
+    msg = Float64MultiArray()
+    # need data in the format [fx,fy, fz, tx, ty, tz] for bicopter we cant do fy or ty, so we will replace with tz or tx
+    horizon = (round(val[0]/240,3)-0.5)
+    vertical = (round(val[1]/160,2)-0.5)
+    temp = [horizon, vertical, val[2], rospy.get_rostime().to_sec()]
+    msg.data = temp
+    pub.publish(msg)
+
+if __name__ == '__main__':
+    mqttClient.on_connect = on_connect
+    mqttClient.on_message = on_message
+    mqttClient.connect("test.mosquitto.org", MQTTPORT)
+    mqttClient.loop_start()
+    while not rospy.is_shutdown():
+        pass 
---
+++ ros_ws/src/crazyswarm/scripts/reception_rpc.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Remote Control - As The Controller Device
+import json, rpc, serial, serial.tools.list_ports, struct, sys, rospy
+from std_msgs.msg import Float64MultiArray
+from datetime import datetime
+
+# color detection remote call
+def exe_color_detection(interface):
+    result = interface.call("color_detection")
+    if result is not None and len(result):
+        res = struct.unpack("<HHH", result)
+        #print("Largest Color Detected: cx={}, cy={}, size={}".format(res[0], res[1], res[2]))
+        msg = Float64MultiArray()
+        # need data in the format [fx,fy, fz, tx, ty, tz] for bicopter we cant do fy or ty, so we will replace with tz or tx
+        horizon = (round(res[0]/240,3)-0.5)
+        vertical = (round(res[1]/160,2)-0.5)
+        temp = [horizon, vertical, res[2], rospy.get_rostime().to_sec()]
+        msg.data = temp
+        pub.publish(msg)
+
+if __name__ == "__main__":
+    pub = rospy.Publisher('openmv', Float64MultiArray, queue_size=1)
+    rospy.init_node('talker', anonymous=True)
+    interface = rpc.rpc_network_master(slave_ip="192.168.0.36", my_ip="192.168.0.54", port=0x1DBA)
+    counter = 0
+    while not rospy.is_shutdown():
+        sys.stdout.flush()
+        exe_color_detection(interface)
