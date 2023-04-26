3e9698d05c8ae778f628f56363cbbcaca7660007
jarvis computer <positronic83@gmail.com>
2023-04-26 11:17:26 -0400

    updating fourflies controls

--- ros_ws/src/crazyswarm/scripts/reception_rpc.py
+++ ros_ws/src/crazyswarm/scripts/reception_rpc.py
@@ -6,23 +6,25 @@ from datetime import datetime
 
 # color detection remote call
 def exe_color_detection(interface):
-    result = interface.call("color_detection")
+    result = interface.call("difference_goal_blob")
     if result is not None and len(result):
         res = struct.unpack("<HHH", result)
         #print("Largest Color Detected: cx={}, cy={}, size={}".format(res[0], res[1], res[2]))
         msg = Float64MultiArray()
         # need data in the format [fx,fy, fz, tx, ty, tz] for bicopter we cant do fy or ty, so we will replace with tz or tx
-        horizon = (round(res[0]/240,3)-0.5)
-        vertical = (round(res[1]/160,2)-0.5)
-        temp = [horizon, vertical, res[2], rospy.get_rostime().to_sec()]
+        horizon = (round(res[0]/160,3)-0.5)
+        vertical = (round(res[1]/120,3)-0.5)
+        temp = [horizon, vertical, res[2]]
         msg.data = temp
         pub.publish(msg)
 
 if __name__ == "__main__":
     pub = rospy.Publisher('openmv', Float64MultiArray, queue_size=1)
     rospy.init_node('talker', anonymous=True)
-    interface = rpc.rpc_network_master(slave_ip="192.168.0.36", my_ip="192.168.0.54", port=0x1DBA)
+    interface = rpc.rpc_network_master(slave_ip="192.168.0.42", my_ip="", port=0x1DBA)
     counter = 0
+    #rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         sys.stdout.flush()
         exe_color_detection(interface)
+        #rate.sleep()
--- ros_ws/src/crazyswarm/scripts/sausage4.py
+++ ros_ws/src/crazyswarm/scripts/sausage4.py
@@ -23,7 +23,7 @@ joy_roll = 0
 joy_thrust = 0
 joy_yaw = 0
 
-
+MVarr = [0,0,0]
 from std_msgs.msg import  Float64MultiArray
 import rospy
 
@@ -60,6 +60,7 @@ class OpenBlimp:
         #print('Connecting to %s' % link_uri)
         
         rospy.Subscriber("/joy", Joy, self.joyChanged)
+        rospy.Subscriber("/openmv", Float64MultiArray, self.openMV)
         print("controller connected")
         
 
@@ -102,6 +103,13 @@ class OpenBlimp:
         
         self.z_d = 0
 
+    
+    def openMV(self, data):
+        global MVarr
+        MVarr[0] = data.data[0]
+        MVarr[1] = data.data[1]
+        MVarr[2] = data.data[2]
+
     def _control(self):
         #self._update_params()
 
@@ -121,7 +129,8 @@ class OpenBlimp:
         go_up = 0
  
         print("starting control")
-
+        direct_shake = 0
+        shake_len = rate*12
         try:
             
             # Unlock startup thrust protection
@@ -143,10 +152,10 @@ class OpenBlimp:
                     tx = 0
                     height_diff = 0
                     
-                    self.allcfs.crazyflies[0].cmdFullState([0,0,0], [0,0,0],[15, 0, 0], 0, [0, 0, 0])
-                    self.allcfs.crazyflies[1].cmdFullState([0,0,0], [0,0,0],[17, 0, 0], 0, [0, 0, 0])
-                    self.allcfs.crazyflies[2].cmdFullState([0,0,0], [0,0,0],[16, 0, 0], 0, [0, 0, 0])
-                    self.allcfs.crazyflies[3].cmdFullState([0,0,0], [0,0,0],[18, 0, 0], 0, [0, 0, 0])
+                    self.allcfs.crazyflies[0].cmdFullState([0,0,0], [0,0,0],[18, 0, 0], 0, [0, 0, 0])
+                    self.allcfs.crazyflies[1].cmdFullState([0,0,0], [0,0,0],[16, 0, 0], 0, [0, 0, 0])
+                    self.allcfs.crazyflies[2].cmdFullState([0,0,0], [0,0,0],[15, 0, 0], 0, [0, 0, 0])
+                    self.allcfs.crazyflies[3].cmdFullState([0,0,0], [0,0,0],[17, 0, 0], 0, [0, 0, 0])
                     # self._cf2.cmdFullState([0,0,0], [0,0,0],[6, 0, 0], 0, [0, 0, 0])
                     # self._cf2.cmdFullState([0,0,0], [0,0,0],[5, 0, 0], 0, [0, 0, 0])
                     # self._cf2.cmdFullState([0,0,0], [0,0,0],[6, 0, 0], 0, [0, 0, 0])
@@ -163,39 +172,53 @@ class OpenBlimp:
                     #fz = self.joy_pad  #up down
                     height_diff += self.joy_pad /rate
 
-                    fx = -self.joy_dx #forward
-                    fy = self.joy_dy
+                    fx = self.joy_dx * 1.2 #forward
+                    fy = self.joy_dy * 1.2
                     tz = self.joy_tauz  #yaw
-                    ty = self.joy_tauy #pitch
-                    tx = - self.joy_taux     #roll
-                    #if self.x_state == 1:
-                    #    self.a_state = 0
-                    #    fx = .1
-                    #    tz = .01
-                    if self.joy_a == 1:
-                        fz = 0
-                        height_diff = 11.2 # 6
-                        '''go_up += 1
-                        if go_up == 5*rate:
-                            if counter%(rate*1) == 0:
-                                fx = .2
-                            else:
-                                fx = -.2 '''
-                    elif self.joy_y== 1:
-                        fz = 0
-                        height_diff = 5
-                    else:
-                        go_up = 0
+
+                    t_angle = 2
+                    ty += self.joy_tauy /rate#pitch #
+                    if ty > t_angle:
+                        ty  = t_angle
+                    tx += self.joy_taux /rate    #roll
+                    if tx > t_angle:
+                        tx  = t_angle
+
+                    if self.joy_x == 1:
+                        print("reset angle")
+                        ty = 0
+                        tx = 0
+                    
+                    if self.a_state == 1:
+                        fx = direct_shake
+                        
+                        if counter%(shake_len) < (shake_len)/2:
+                            if counter%10 == 0:
+                                print("shake left")
+                            direct_shake = .6
+                        else:
+                            if counter%10 == 0:
+                                print("shake right")
+                            direct_shake = -.6
+                                
+                    if  self.joy_y== 1:
+                        tz = MVarr[0]*10
+                        
+                        if counter%40 == 0:
+                            print("vision on")
+                        #tz = MVarr[1]
+                        #fx = MVarr[2]
+
 
                         
                        
                         
 
 
-                    self.allcfs.crazyflies[0].cmdFullState([fx, fy, fz], [tx, ty, tz],[11, 0, height_diff], 0, [0, 0, 0])
-                    self.allcfs.crazyflies[1].cmdFullState([fx, fy, fz], [tx, ty, tz],[13, 0, height_diff], 0, [0, 0, 0])
-                    self.allcfs.crazyflies[2].cmdFullState([fx, fy, fz], [tx, ty, tz],[12, 0, height_diff], 0, [0, 0, 0])
-                    self.allcfs.crazyflies[3].cmdFullState([fx, fy, fz], [tx, ty, tz],[14, 0, height_diff], 0, [0, 0, 0])
+                    self.allcfs.crazyflies[0].cmdFullState([fx, fy, fz], [tx, ty, tz],[14, 0, height_diff], 0, [0, 0, 0])#8
+                    self.allcfs.crazyflies[1].cmdFullState([fx, fy, fz], [tx, ty, tz],[12, 0, height_diff], 0, [0, 0, 0])#2
+                    self.allcfs.crazyflies[2].cmdFullState([fx, fy, fz], [tx, ty, tz],[11, 0, height_diff], 0, [0, 0, 0])#4
+                    self.allcfs.crazyflies[3].cmdFullState([fx, fy, fz], [tx, ty, tz],[13, 0, height_diff], 0, [0, 0, 0])#6
                     # self._cf2.cmdFullState([fx, fy, fz], [tx, ty, tz],[4, 0, 0], 0, [0, 0, 0])
                     # self._cf3.cmdFullState([fx, fy, fz], [tx, ty, tz],[4, 0, 0], 0, [0, 0, 0])
                     # self._cf4.cmdFullState([fx, fy, fz], [tx, ty, tz],[4, 0, 0], 0, [0, 0, 0])
@@ -203,9 +226,9 @@ class OpenBlimp:
                     
 
 
-                    if counter%20 == 0:
+                    if counter%40 == 0:
                         print(fx, fy, fz, tx, ty, tz)
-                        counter = 0
+                        
                     counter += 1
                 
                 
@@ -232,17 +255,17 @@ class OpenBlimp:
         if lastData != None:    
             if self.joy_x == 1 and lastData.buttons[2] == 0:
                 self.x_state = not self.x_state
-                print("afk state", self.x_state)
+                #print("afk state", self.x_state)
             if self.joy_b == 1 and lastData.buttons[1] == 0:
                 self.b_state = not self.b_state
-                print("swap", self.b_state)
+                #print("swap", self.b_state)
 
             if self.joy_a == 1 and lastData.buttons[0] == 0:
                 self.a_state = not self.a_state
-                print("up state", self.a_state)
+                #print("up state", self.a_state)
             if self.joy_y == 1 and lastData.buttons[3] == 0:
                 self.y_state = not self.y_state
-                print("rest flight", self.y_state)
+                #print("rest flight", self.y_state)
 
             self.joy_x = lastData.buttons[2]
             self.joy_y = lastData.buttons[3]
@@ -256,11 +279,11 @@ class OpenBlimp:
             self.joy_tauz = lastData.axes[0] 
             
             
-            self.joy_tauy   = lastData.axes[7] # dz aka height
-            self.joy_taux = lastData.axes[6] # taux aka roll
+            self.joy_tauy   = lastData.axes[6] # dz aka height
+            self.joy_taux = lastData.axes[7] # taux aka roll
 
-            self.joy_dx   = lastData.axes[4] #dx
-            self.joy_dy = lastData.axes[3] #dy
+            self.joy_dx   = lastData.axes[3] #dx
+            self.joy_dy = lastData.axes[4] #dy
             
 
 
